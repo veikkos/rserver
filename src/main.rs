670e2d7e62466fb68a3e9f779453b95266480b9d
Veikko Soininen <veikko.soininen@gmail.com>
2022-06-29 00:09:48 +0300

    Add some asset files and return those dynamically

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,53 @@
 use rouille::input;
 use rouille::Request;
 use rouille::Response;
+use std::fs;
+use std::path::Path;
 
 const PORT: i32 = 80;
 
-fn handle_after_login(_request: &Request) -> Response {
-    Response::text("You are in a secret area")
+fn get_asset_path(file: String) -> String {
+    format!("./assets/{}", file)
+}
+
+fn get_html_response(file: String) -> Response {
+    println!("html - {}", file);
+    let contents = fs::read_to_string(get_asset_path(file));
+
+    let contents = match contents {
+        Ok(file) => file,
+        Err(_error) => return Response::empty_404()
+    };
+
+    Response::html(&contents)
+}
+
+fn get_binary_response(file: String) -> Response {
+    println!("bin - {}", file);
+    let contents = fs::read(get_asset_path(file));
+
+    let contents = match contents {
+        Ok(file) => file,
+        Err(_error) => return Response::empty_404()
+    };
+
+    Response::from_data("application/octet-stream", contents)
+}
+
+fn handle_after_login(request: &Request) -> Response {
+    let url = request.url();
+    let file = match url.as_str(){
+        "/" => "index.html".to_owned(),
+        _ => (&url[1..url.len()]).to_string(),
+    };
+    let ext = Path::new(&file).extension();
+    match ext {
+        Some(x) => match x.to_str().unwrap() {
+            "html" => return get_html_response(file),
+            _ => return get_binary_response(file)
+        },
+        None => return Response::empty_400(),
+    };
 }
 
 fn main() {
